a7a0fde63d98eac42592977827d56d4c44e1b808
Guillermo Rebolledo <gortiz.dev+dev@gmail.com>
2026-09-14 15:46:35 -0600

    Update production domain to financebuddy.tech

--- FinanceBuddy/Shared/Website.swift
+++ FinanceBuddy/Shared/Website.swift
@@ -2,6 +2,6 @@ import SafariServices
 import SwiftUI
 
 enum Website {
-  static let url = URL(string: "https://finance-buddy-self.vercel.app")!
+  static let url = URL(string: "https://financebuddy.tech")!
   static let dashboard = url.appending(path: "dashboard")
 }
